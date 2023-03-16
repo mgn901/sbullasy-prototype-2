001d0c6487b6843d534b5933feb9a456ecc25a17
mgn901 <mgn901+dev@gmail.com>
2023-03-16 17:58:27 +0900

    Initial Commit - Add ignore files - Add files to build the app - Add README.md and LICENSE

---
+++ Dockerfile
@@ -0,0 +1,43 @@
+# stage: deps
+
+FROM node:18-slim as deps
+WORKDIR /usr/src/app
+# 1. Install deps needed to run the app
+COPY ./package.json .
+RUN npm install --omit=dev
+
+# stage: builder
+
+FROM node:18-slim as builder
+WORKDIR /usr/src/app
+# 1. Install dev-dependencies
+COPY --from=deps /usr/src/app/node_modules ./node_modules
+COPY ./package.json .
+RUN npm install
+# 2. Copy files needed to build the app
+COPY ./scripts ./scripts
+COPY ./src ./src
+COPY tailwind.config.js .
+COPY tsconfig*.json ./
+# 3. Build the app
+ENV NODE_ENV=production
+RUN sed -i -z -E 's/  \"exports\"\:.*\n.*\n.*\n.*\n.*\n//mg' ./node_modules/esbuild-plugin-inline-worker/package.json
+RUN npm run build
+
+# stage: runtime
+
+FROM node:18-slim as runtime
+WORKDIR /usr/src/app
+# 1. Copy files needed to run the app
+# COPY --chown=node:node --from=deps /usr/src/app/node_modules ./node_modules
+COPY --chown=node:node --from=builder /usr/src/app/package.json .
+COPY --chown=node:node --from=builder /usr/src/app/production ./production
+# 2. Install dumb-init
+RUN apt-get update\
+	&& apt-get install -y --no-install-recommends dumb-init\
+	&& apt-get -y clean\
+	&& rm -rf /var/lib/apt/lists/*
+# 3. Run the app
+USER node
+EXPOSE 80
+CMD ["dumb-init", "node", "./production/server/index.js"]
---
+++ scripts/build.ts
@@ -0,0 +1,69 @@
+import esbuild from 'esbuild';
+import inlineWorkerPlugin from 'esbuild-plugin-inline-worker';
+import stylePlugin from 'esbuild-style-plugin';
+import tailwindcss from 'tailwindcss';
+const tailwindConfig = require('../tailwind.config.js');
+const autoprefiexer = require('autoprefixer');
+const copyPlugin = require('esbuild-copy-static-files');
+const packageJSON = require('../package.json');
+
+const NODE_ENV = process.env.NODE_ENV ?? 'development';
+const APP_VERSION = process.env.APP_VERSION ?? packageJSON.version as string ?? '1.0.0';
+const isDev = NODE_ENV === 'development';
+const outDir = NODE_ENV;
+
+const optionsClient: esbuild.BuildOptions = {
+	entryPoints: [
+		'src/client/index.tsx',
+		// 'src/client/serviceWorker.ts',
+	],
+	outdir: `${outDir}/client`,
+	assetNames: 'assets/[name]',
+	loader: {
+		'.woff': 'file',
+		'.woff2': 'file',
+	},
+	bundle: true,
+	define: {
+		'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+		'process.env.APP_VERSION': JSON.stringify(APP_VERSION),
+	},
+	minify: !isDev,
+	sourcemap: isDev,
+	target: ['chrome98', 'firefox97', 'edge98', 'safari14'],
+	plugins: [
+		inlineWorkerPlugin(),
+		stylePlugin({
+			postcss: {
+				plugins: [tailwindcss(tailwindConfig), autoprefiexer()],
+			},
+		}),
+		copyPlugin({
+			src: 'src/client/static',
+			dest: `${outDir}/client`,
+		}),
+	],
+};
+
+const optionsServer: esbuild.BuildOptions = {
+	entryPoints: [
+		'src/server/index.ts',
+	],
+	outdir: `${outDir}/server`,
+	platform: 'node',
+	bundle: true,
+	define: {
+		'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+	},
+	minify: !isDev,
+	sourcemap: isDev,
+	target: ['node18'],
+};
+
+const start = () => {
+	const contextClient = esbuild.build(optionsClient);
+	const contextServer = esbuild.build(optionsServer);
+	return;
+}
+
+start();
