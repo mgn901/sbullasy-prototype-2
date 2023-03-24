2758ebdd0f52f6d2322f7e20c04abf9131ea260e
mgn901 <mgn901+dev@gmail.com>
2023-03-25 00:13:37 +0900

    IRepository#findBy* may be undefined

--- src/server/IRepository.ts
+++ src/server/IRepository.ts
@@ -2,7 +2,7 @@ import { Entity } from './Entity';
 import { EntityAsync } from './EntityAsync';
 
 export interface IRepository<Interface extends Entity> {
-	findByID(id: Interface['id']): Promise<EntityAsync<Interface>>;
+	findByID(id: Interface['id']): Promise<EntityAsync<Interface>> | undefined;
 	findByIDs(...ids: Interface['id'][]): Promise<EntityAsync<Interface>[]>;
 	save(item: EntityAsync<Interface> | Interface): Promise<void>;
 	deleteByID(id: string): Promise<void>;
--- src/server/user-tag-request/IUserTagRequestRepository.ts
+++ src/server/user-tag-request/IUserTagRequestRepository.ts
@@ -5,7 +5,7 @@ import { IUser } from '../user/IUser';
 import { IUserTagRequest } from './IUserTagRequest';
 
 export type IUserTagRequestRepository = IRepository<IUserTagRequest> & {
-	findByToken(token: IUserTagRequest['token']): Promise<EntityAsync<IUserTagRequest>>;
+	findByToken(token: IUserTagRequest['token']): Promise<EntityAsync<IUserTagRequest>> | undefined;
 	findByUserAndTag(user: IUser['id'], tag: IUserTag['id']): Promise<EntityAsync<IUserTagRequest>[]>;
 	deleteByUserAndTag(user: IUser['id'], tag: IUserTag['id']): Promise<void>;
 };
