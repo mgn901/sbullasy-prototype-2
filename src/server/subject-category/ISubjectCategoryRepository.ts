8ee9f97bb2899881c96295623389db20a2386659
mgn901 <mgn901+dev@gmail.com>
2023-03-30 16:42:50 +0900

    Add findAll() to ISubjectCategoryRepository

--- src/server/subject-category/ISubjectCategoryRepository.ts
+++ src/server/subject-category/ISubjectCategoryRepository.ts
@@ -1,4 +1,7 @@
+import { EntityAsync } from '../EntityAsync';
 import { IRepository } from '../IRepository';
 import { ISubjectCategory } from './ISubjectCategory';
 
-export type ISubjectCategoryRepository = IRepository<ISubjectCategory>;
+export type ISubjectCategoryRepository = IRepository<ISubjectCategory> & {
+	findAll(): Promise<EntityAsync<ISubjectCategory>[]>;
+};
--- src/server/subject-category/SubjectCategoryRepository.kysely.ts
+++ src/server/subject-category/SubjectCategoryRepository.kysely.ts
@@ -6,18 +6,26 @@ import { ISubjectCategoryRepository } from './ISubjectCategoryRepository';
 export class SubjectCategoryRepository implements ISubjectCategoryRepository {
 
 	public async findByID(id: string): Promise<EntityAsync<ISubjectCategory> | undefined> {
-		const subjects = await this.findByIDs(id);
-		const subject = subjects[0];
-		return subject;
+		const categories = await this.findByIDs(id);
+		const category = categories[0];
+		return category;
 	}
 
 	public async findByIDs(...ids: string[]): Promise<EntityAsync<ISubjectCategory>[]> {
-		const category = await db
+		const categories = await db
 			.selectFrom('categories')
 			.where('id', 'in', ids)
 			.selectAll()
 			.execute();
-		return category;
+		return categories;
+	}
+
+	public async findAll(): Promise<EntityAsync<ISubjectCategory>[]> {
+		const categories = await db
+			.selectFrom('categories')
+			.selectAll()
+			.execute();
+		return categories;
 	}
 
 	public async save(item: ISubjectCategory | EntityAsync<ISubjectCategory>): Promise<void> {
