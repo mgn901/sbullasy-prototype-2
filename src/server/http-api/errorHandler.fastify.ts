79eddd8b788de70e3919309591f6d4150813ef6c
mgn901 <mgn901+dev@gmail.com>
2023-04-26 14:46:09 +0900

    Fix: errorHandler mustn't expose call-stack

--- src/server/http-api/errorHandler.fastify.ts
+++ src/server/http-api/errorHandler.fastify.ts
@@ -8,25 +8,35 @@ import { TokenInvalidError } from '../error/TokenInvalidError';
 import { TooManyRequestsError } from '../error/TooManyRequestsError';
 import { WrongParamsError } from '../error/WrongParamsError';
 
+const getStatusByError = (error: SbullasyError) => {
+	if (error instanceof NotFoundError)
+		return 404;
+	else if (error instanceof PermissionError)
+		return 403;
+	else if (error instanceof RequestExpiredError)
+		return 403;
+	else if (error instanceof SessionInvalidError)
+		return 401;
+	else if (error instanceof TokenInvalidError)
+		return 401;
+	else if (error instanceof WrongParamsError)
+		return 400;
+	else if (error instanceof TooManyRequestsError)
+		return 429;
+	else
+		return 500;
+}
+
 export const errorHandler: Parameters<FastifyInstance['setErrorHandler']>[0] = async (error, request, reply) => {
-	if (error instanceof NotFoundError) {
-		await reply.status(404).send(error);
-	} else if (error instanceof PermissionError) {
-		await reply.status(403).send(error);
-	} else if (error instanceof RequestExpiredError) {
-		await reply.status(403).send(error);
-	} else if (error instanceof SessionInvalidError) {
-		await reply.status(401).send(error);
-	} else if (error instanceof TokenInvalidError) {
-		await reply.status(401).send(error);
-	} else if (error instanceof WrongParamsError) {
-		await reply.status(400).send(error);
-	} else if (error instanceof TooManyRequestsError) {
-		await reply.status(429).send(error);
-	} else if (error instanceof SbullasyError) {
-		await reply.status(500).send(error);
+	if (error instanceof SbullasyError) {
+		const response = {
+			name: error.name,
+			message: error.message,
+		};
+		const statusCode = getStatusByError(error);
+		await reply.status(statusCode).send(response);
 	} else {
-		const response: Error = {
+		const response = {
 			name: 'UnexpectedInternalError',
 			message: 'The server could not process your request.',
 		};
